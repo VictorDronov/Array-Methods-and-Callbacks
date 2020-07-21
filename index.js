1a062e50f32593d98e1cec1804822547e6233698
victor dronov <victordronov1@gmail.com>
2020-07-21 17:16:20 -0400

    task 1 done, the easier way

--- index.js
+++ index.js
@@ -12,23 +12,18 @@ console.log-ing the following pieces of data
 (c) Home Team goals for 2014 world cup final
 (d) Away Team goals for 2014 world cup final
 (e) Winner of 2014 world cup final */
-let homeTeam = fifaData.filter(function(item){
+
+fifaData.map(function(item,index){
     if (item["Year"] === 2014  && item["Stage"] === "Final"){
-    return item["Home Team Name"]
+        console.log(index)
     }
 });
-console.log(homeTeam)
 
-let awayTeam = fifaData.filter(function(item){
-    if (item["Year"] === 2014  && item["Stage"] === "Final"){
-    return item["Away Team Name"]
-    }
-});
-console.log(awayTeam)
-
-console.log(fifaData[828])
-console.log(fifaData[828])
-console.log(fifaData[828])
+console.log(fifaData[828]["Home Team Name"])
+console.log(fifaData[828]["Away Team Name"])
+console.log(fifaData[828]["Home Team Goals"])
+console.log(fifaData[828]["Away Team Goals"])
+console.log(fifaData[828]["Win conditions"])
 
 /* Task 2: Create a function called  getFinals that takes `data` as an 
 argument and returns an array of objects with only finals data */
